b2052304e5f1ca79a7bdfb922f8be61787e47c13
girot45 <98813117+girot45@users.noreply.github.com>
2024-01-21 15:06:33 +0300

    fixes and updates

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
+from database.Database import db_conn
 pygame.init()
 
 if __name__ == "__main__":
     from game_options.Game import Game
-    game = Game()
-    game.run_game()
+    player = db_conn.create_player("Kirill")
+    game_id = db_conn.add_game_session(player)
+    if game_id is not None:
+        game = Game(game_id=game_id, player=player)
+        game.run_game()
+    else:
+        print("Пользователя нет")
